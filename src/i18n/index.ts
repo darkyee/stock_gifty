3fbd20039e45bbccce8db3691a64adfb40965ed2
Darkyee <52757313+darkyee@users.noreply.github.com>
2024-12-08 10:18:41 -0600

    Update src/i18n/index.ts [BoltSync]

---
+++ src/i18n/index.ts
@@ -0,0 +1,29 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import enTranslations from './locales/en';
+import esTranslations from './locales/es';
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: {
+        translation: enTranslations,
+      },
+      es: {
+        translation: esTranslations,
+      },
+    },
+    fallbackLng: 'en',
+    detection: {
+      order: ['navigator', 'htmlTag', 'path', 'subdomain'],
+      caches: ['localStorage'],
+    },
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+export default i18n;
